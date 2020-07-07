b6afcff251881a6cafb060c375626b339213cae8
MultiApollo19 <mariusz.belwon@gmail.com>
2020-07-08 00:39:24 +0200

    Revert ".net 4.5"
    This reverts commit f6aabf9b32dd05506bdb5ed276733e504fbfde4e.

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -32,7 +32,7 @@ namespace TestApp
         {
             
             lbl1.Text = "Sprawdzam dostępność aktualizacji...";
-            string serverVersion = "http://wickedlauncher.5v.pl/launcher/meta.dbg";
+            string serverVersion = "https://doc-0c-5c-docs.googleusercontent.com/docs/securesc/nke845s47lsc0f9ldr2m9gapal47qm72/f8em3jk6bqf5cc310r3uvhfii1ppmcrv/1580497200000/11875819559820862250/11875819559820862250/1mUaw4m5ViMymKZn0HPlw9CxFRhCH31hP?e=download&gd=true&access_token=ya29.Il-8BzgEcMwRg-sqYFAYfuwvEpxD8i9WrESqAi0MIRs0NPHtXHIzxY3YDv610oB4dw2_jJ1etb8rdK5dYwUrpfCwxuCDFNcXGq3O8W8Z4OvIuBef-Cx4yY4nTNlDqkdLJg";
             string currentVersion = Directory.GetCurrentDirectory() + "/meta.dbg";
             //current
             string currentRead = File.ReadAllText(currentVersion);
@@ -64,7 +64,7 @@ namespace TestApp
             string currentVersion = Directory.GetCurrentDirectory() + "/meta.dbg";
             Meta meta = new Meta();
             meta.version = new Version("0.1.0.0");
-            meta.url = "http://wickedlauncher.5v.pl/launcher/meta.dbg";
+            meta.url = "https://doc-14-5c-docs.googleusercontent.com/docs/securesc/nke845s47lsc0f9ldr2m9gapal47qm72/65rvtprl8nsrdqo6dfsvj16fhgtcitc2/1580496300000/11875819559820862250/11875819559820862250/1SWwA0cFmE5q97ky2q2EUroe8dzlG3ukE?e=download&gd=true&access_token=ya29.Il-8BzgEcMwRg-sqYFAYfuwvEpxD8i9WrESqAi0MIRs0NPHtXHIzxY3YDv610oB4dw2_jJ1etb8rdK5dYwUrpfCwxuCDFNcXGq3O8W8Z4OvIuBef-Cx4yY4nTNlDqkdLJg";
             string write = JsonConvert.SerializeObject(meta);
             StreamWriter file = File.CreateText(currentVersion);
             file.Write(write);
